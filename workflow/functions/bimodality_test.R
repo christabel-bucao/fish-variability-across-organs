3c46d37fd38215f13dfa6b716c05e22a4fb79e84
Christabel Bucao <christa.bucao@gmail.com>
2025-08-30 21:03:10 +0200

    Call matrixStats explicitly for matrix computations to avoid error message

--- workflow/functions/bimodality_test.R
+++ workflow/functions/bimodality_test.R
@@ -8,9 +8,9 @@ library(matrixStats)
 #### Functions ####
 
 # Convert expression matrix to z-score matrix
-zscore <- function(expr.matrix) {
+compute_zscore <- function(expr.matrix) {
   means <- rowMeans(expr.matrix, na.rm=TRUE)
-  sds <- rowSds(expr.matrix, useNames=TRUE, na.rm=TRUE)
+  sds <- matrixStats::rowSds(expr.matrix, useNames=TRUE, na.rm=TRUE)
   
   zscore.matrix <- (expr.matrix - means) / sds
   
