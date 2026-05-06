fd3c0c210646ebc702b5930621e1fff55d15d073
Christabel Bucao <christa.bucao@gmail.com>
2026-05-06 17:21:38 +0200

    Add cameraPR analysis

--- workflow/run_go_figure.sh
+++ workflow/run_go_figure.sh
@@ -3,7 +3,8 @@
 
 cd ../bin/GO-Figure 
 
-InputDir=${1} 
+InputDir=${1}
+Ontology=${2} 
 
 mkdir -p ${1}/go-figure
 OutputDir=${1}/go-figure
@@ -15,8 +16,9 @@ do
 	./gofigure-mac --input ${GOTerms} --input_type standard-plus \
 		--output ${OutputDir} --outfile_appendix "${FileName%.txt}" \
 		--size members --colours user --sort_by user-descending \
-		--colour_label "Number of conditions" --max_label 20 --ontology bpo 
+		--colour_label "Number of conditions" --max_label 20 --ontology ${Ontology} 
 done
 
 # To run:
 #./run_go_figure.sh ../../results/07_GO_Enrichment/terms
+#./run_go_figure.sh ../../results/07b_GO_cameraPR/no_combat/full_qc/terms all
